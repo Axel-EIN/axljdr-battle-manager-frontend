26cdb2061b355a43abdeb12b759a494bd5f3f446
Axel <axel.ein@gmail.com>
2024-08-01 20:08:47 +0200

    [HEADER] added user connected message

--- src/components/templates/header.jsx
+++ src/components/templates/header.jsx
@@ -1,11 +1,41 @@
-import { Link } from 'react-router-dom';
+import { useContext, useEffect } from 'react';
+import { useNavigate, Link, } from 'react-router-dom';
+import { ContexteUtilisateur } from '../../contexts/contexteUtilisateur';
 
 function header() {
+  const { utilisateur, deconnecterUtilisateur } = useContext(ContexteUtilisateur);
+  const navigate = useNavigate();
+
+  useEffect( () => {}, [utilisateur] );
+
+  const gererDeconnexion = () => {
+    deconnecterUtilisateur();
+    navigate('/');
+  };
+
+  console.log('header composant variable utilisateur :', utilisateur);
+
   return (
     <>
       <h2>Ceci est l'en-tête</h2>
-      <Link to="/">Accueil</Link>
-      <Link to="/connexion">Se connecter</Link>
+      <nav>
+        <Link to="/">Accueil</Link>
+        <Link to="/connexion">Se connecter</Link>
+        <Link to="/inscription">S'inscrire</Link>
+      </nav>
+      <br/>
+
+      {utilisateur ? (
+        <>
+          Bienvenue sur AXL-JDR-BATTLE-MANAGER <strong>{utilisateur.prenom}</strong> !
+          <br/>
+          <button onClick={gererDeconnexion}>Se deconnecter</button>
+        </>
+      ) : (
+        <>
+          Vous n'êtes pas connecté !
+        </>
+      ) }
     </>
   )
 }
