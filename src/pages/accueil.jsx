c1a01c6bd0b1527921551093b0d892c2d13c205a
Axel <axel.ein@gmail.com>
2024-08-01 18:12:21 +0200

    [HOME page] removed login form

--- src/pages/accueil.jsx
+++ src/pages/accueil.jsx
@@ -20,34 +20,9 @@ function accueil() {
     setCombats(data);
   };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const credentials = {
-      identifiant: identifiant,
-      mdp: mdp
-    }
-
-    try {
-      await axios.post(URLS.USER_LOGIN, credentials);
-      navigate("/");
-    } catch (erreur) {
-      console.error(erreur.message);
-    }
-  }
-
   return (
     <>
       <h1>Ceci est la page d'accueil</h1>
-
-      <h2>Connexion :</h2>
-
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="identifiant" id="identifiant" value={identifiant} onChange={(event) => setIdentifiant(event.target.value)} />
-        <br />
-        <input type="password" name="mdp" id="mdp" value={mdp} onChange={(event) => setMdp(event.target.value)} />
-        <br />
-        <button>Se connecter</button>
-      </form>
 
       <h2>Combats :</h2>
 
