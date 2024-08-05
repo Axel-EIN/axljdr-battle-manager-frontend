f6478c9a3698c003ef3964ffcc417d823569b24b
Axel <axel.ein@gmail.com>
2024-08-05 13:16:45 +0200

    [TEMPLATE] added container

--- src/components/templates/template.jsx
+++ src/components/templates/template.jsx
@@ -1,16 +1,19 @@
 import { Outlet } from "react-router-dom";
-import Header from './Header.jsx';
-import Footer from './Footer.jsx';
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import "./template.css";
 
 const Template = () => {
-    return (
-        <>
-            <Header />
-            <main>
-                <Outlet />
-            </main>
-            <Footer />
-        </>
-    );
-}
+  return (
+    <>
+      <Header />
+      <main>
+        <container className="container">
+            <Outlet />
+        </container>
+      </main>
+      <Footer />
+    </>
+  );
+};
 export default Template;
