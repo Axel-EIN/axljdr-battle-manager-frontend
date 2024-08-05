c72dd2676103a578e8ce5c4cf6d01563532e6106
Axel <axel.ein@gmail.com>
2024-08-05 13:13:42 +0200

    [FOOTER] updated

--- src/components/templates/footer.jsx
+++ src/components/templates/footer.jsx
@@ -1,7 +1,11 @@
+import './footer.css';
+
 function Footer() {
   return (
-    <h2>Pied-de-page</h2>
-  )
+    <footer>
+      <h2>Pied-de-page</h2>
+    </footer>
+  );
 }
 
 export default Footer;
