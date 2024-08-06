a67819d1a377fa5f280f1b2485a37ca556e5a4e7
Axel <axel.ein@gmail.com>
2024-08-06 15:47:25 +0200

    [CONTEXT user] removed localstorage, added getCurrentUser

--- src/contexts/contexteUtilisateur.jsx
+++ src/contexts/contexteUtilisateur.jsx
@@ -8,55 +8,51 @@ export const FournisseurUtilisateur = ({ children }) => {
   const [utilisateur, setUtilisateur] = useState(null);
 
   useEffect(() => {
-    recupererUtilisateurLocal();
+    recupererUtilisateurCourant();
   }, []);
 
-  // Fonction pour récupérer l'Utilisateur enregistré dans le Storage Local
-  const recupererUtilisateurLocal = async () => {
+  // Fonction pour récupérer l'Utilisateur Courant déjà authentifié
+  const recupererUtilisateurCourant = async () => {
     try {
-      // récupération du champ "utilisateur" dans le storage local
-      const utilisateurLocal = localStorage.getItem("utilisateur");
+      const { data, status } = await axios.get(URLS.USER_CURRENT, {
+        withCredentials: true // Permet d'envoyer le cookier jwt lors de la requête
+      });
 
-      if (utilisateurLocal != null) // Si l'utilisateur existe
-        setUtilisateur(JSON.parse(utilisateurLocal)); // On lit la chaîne et la transforme en JSON et met à jour le state utilisateur
+      console.log("data", data);
+
+      if ( status == 200 ) {
+        const utilisateurCourant = data;
+        setUtilisateur(utilisateurCourant);
+      }
+
     } catch (error) {
       console.error("error", error);
     }
   };
 
-  // Fonction pour connecter l'Utilisateur et mettre à jour le Storage Local
+  // Fonction pour connecter l'Utilisateur pas encore authentifié
   const connecterUtilisateur = async (identifiant, mdp) => {
     
-    // Requête pour connecter l'utilisateur
     const { data, status } = await axios.post(URLS.USER_LOGIN, {
       identifiant: identifiant,
       mdp: mdp,
+    }, {
+      withCredentials: true
     });
 
     if ( status === 200 ) {
       const utilisateurConnecte = data;
       setUtilisateur(utilisateurConnecte);
-      // On crée un champ utilisateur dans le storage local en le transformant en chaîne
-      localStorage.setItem("utilisateur", JSON.stringify(utilisateurConnecte));
-      return utilisateurConnecte; // On renvoit l'utilisateur
     }
-
   };
 
-  // Fonction pour déconnecter l'Utilisateur et mettre à jour le Storage Local
+  // Fonction pour déconnecter l'Utilisateur
   const deconnecterUtilisateur = () => {
     setUtilisateur(null);
-    localStorage.removeItem("utilisateur");
-  };
-
-  // Fonction pour inscrire l'Utilisateur
-  const inscrireUtilisateur = async (donneesUtilisateur) => {
-    const nouvelUtilisateur = await axios.post(URLS.USER_REGISTER, donneesUtilisateur);
-    setUtilisateur(nouvelUtilisateur);
   };
 
   return (
-    <ContexteUtilisateur.Provider value={{ utilisateur, connecterUtilisateur, deconnecterUtilisateur, inscrireUtilisateur }}>
+    <ContexteUtilisateur.Provider value={{ utilisateur, connecterUtilisateur, deconnecterUtilisateur }}>
       {children}
     </ContexteUtilisateur.Provider>
   );
