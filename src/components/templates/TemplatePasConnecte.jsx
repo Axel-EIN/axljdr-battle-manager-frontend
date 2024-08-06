ee19c953f214786e12f0828a2f32724f0527ea08
Axel <axel.ein@gmail.com>
2024-08-06 18:08:06 +0200

    [TEMPLATE notlogged] implemented loading state

--- src/components/templates/TemplatePasConnecte.jsx
+++ src/components/templates/TemplatePasConnecte.jsx
@@ -1,28 +1,21 @@
-import Template from './Template.jsx';
-import { ContexteUtilisateur } from '../../contexts/contexteUtilisateur.jsx';
-import { useContext } from 'react';
-import { Navigate } from 'react-router-dom';
+import Template from "./Template.jsx";
+import { ContexteUtilisateur } from "../../contexts/contexteUtilisateur.jsx";
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
 
 const TemplatePasConnecte = () => {
+  const { utilisateur, loading } = useContext(ContexteUtilisateur); // Récupération de l'utilsiateur connecté dans le contexte et de la variable loading
 
-    // Récupération de l'utilsiateur connecté dans le contexte
-    const { utilisateur } = useContext(ContexteUtilisateur);
+  // Si l'utilisateur n'existe pas, en se rendant sur des pages pour non-connecté, on laisse passer et on affiche le template header outlet footer
+  // Sinon cela veut dire que l'utilisateur est connecté et il ne doit pas avoir accès à ces pages, on le redirige vers la page mon-compte
 
-    // Si l'utilisateur n'existe pas, en se rendant sur des pages pour non-connecté, on laisse passer et on affiche le template header outlet footer
-    // Sinon cela veut dire que l'utilisateur est connecté et il ne doit pas avoir accès à ces pages, on le redirige vers la page mon-compte
+  return (
+    <>
+      {!loading && (
+        <>{!utilisateur ? <Template /> : <Navigate to="/mon-compte" />}</>
+      )}
+    </>
+  );
+};
 
-    return (
-        <>
-            {!utilisateur ?
-                (
-                    <Template/>
-                )
-                :
-                ( 
-                    <Navigate to="/mon-compte"/> 
-                )
-            }
-        </>
-    );
-}
 export default TemplatePasConnecte;
