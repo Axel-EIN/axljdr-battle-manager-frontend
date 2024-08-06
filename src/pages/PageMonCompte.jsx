9bc8bdfa78bbc4da003b16a31176ef3edf534b78
Axel <axel.ein@gmail.com>
2024-08-06 15:48:01 +0200

    [MYACCOUNT page] updated

--- src/pages/PageMonCompte.jsx
+++ src/pages/PageMonCompte.jsx
@@ -1,8 +1,25 @@
+import { useContext } from "react";
+import { ContexteUtilisateur } from "../contexts/contexteUtilisateur";
+
 const PageMonCompte = () => {
+    const { utilisateur } = useContext(ContexteUtilisateur);
 
     return (
         <>
-            <h1>Bienvenue sur votre page de compte !</h1>
+            <h1>Bienvenue <strong>{utilisateur.prenom}</strong>, vous êtes sur votre page de compte !</h1>
+
+            <h2>Vos informations</h2>
+
+            Votre Identifiant : <strong>{utilisateur.identifiant}</strong>
+            <br/><br/>
+            Votre Email : <strong>{utilisateur.email}</strong>
+            <br/><br/>
+            Votre Prénom : <strong>{utilisateur.prenom}</strong>
+            <br/><br/>
+            Votre Rôle : <strong>{utilisateur.role}</strong>
+            <br/><br/>
+            Votre Avatar : <strong>{utilisateur.avatar}</strong>
+            <br/><img src={utilisateur.avatar} alt="Avatar de l'Utilisateur" />
         </>
     );
 };
