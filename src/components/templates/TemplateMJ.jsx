e4c3d02475410659dfbc95f3705403025e7c3c1e
Axel <axel.ein@gmail.com>
2024-08-06 18:23:16 +0200

    [TEMPLATE mj] added

---
+++ src/components/templates/TemplateMJ.jsx
@@ -0,0 +1,20 @@
+import Template from "./Template.jsx";
+import { ContexteUtilisateur } from "../../contexts/contexteUtilisateur.jsx";
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+
+const TemplateMJ = () => {
+  const { utilisateur, loading } = useContext(ContexteUtilisateur); // Récupération de l'utilsiateur connecté dans le contexte et de la variable loading
+
+  // Si le loading de l'utilisateur du context et terminé, alors on verifie si l'utilisateur existe.
+  // Si oui on affiche le template, sinon on redirige vers la page de connexion
+  return (
+    <>
+      {!loading && (
+        <>{utilisateur && utilisateur.role == 'mj' ? <Template /> : <Navigate to="/connexion" />}</>
+      )}
+    </>
+  );
+};
+
+export default TemplateMJ;
