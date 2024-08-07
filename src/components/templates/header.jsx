17b019750b973532ed8f1fa988ad038ab66e3196
Axel <axel.ein@gmail.com>
2024-08-07 15:10:21 +0200

    [HEADER] added login button

--- src/components/templates/header.jsx
+++ src/components/templates/header.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 function header() {
   return (
     <>
       <h2>Ceci est l'en-tête</h2>
       <Link to="/">Accueil</Link>
+      <Link to="/connexion">Se connecter</Link>
     </>
   )
 }
