5c7b13c8eb32e557018fc08936e737ebd8048974
Axel <axel.ein@gmail.com>
2024-08-07 16:25:53 +0200

    [APP] renamed login route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import PageConnexion from './pages/PageConnexion.jsx';
 import PageInscription from './pages/PageInscription.jsx';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
     <Routes>
       <Route path="/" element={<Template />}>
+        <Route path="/connexion" element={<PageConnexion />} />
         <Route path="/inscription" element={<PageInscription />} />
       </Route>
     </Routes>
