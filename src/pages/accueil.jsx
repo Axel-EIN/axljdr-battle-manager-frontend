a17e3f75f2af95a55bce4139a38975f51382af66
Axel <axel.ein@gmail.com>
2024-08-07 15:17:41 +0200

    [HOME page] added a login form

--- src/pages/accueil.jsx
+++ src/pages/accueil.jsx
@@ -1,23 +1,54 @@
-import React, { useEffect } from 'react';
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { URLS } from './../constants/urls.js';
+import { useNavigate } from "react-router-dom";
 
 function accueil() {
   const [combats, setCombats] = useState([]);
+  const [identifiant, setIdentifiant] = useState([]);
+  const [mdp, setMdp] = useState([]);
+  const navigate = useNavigate();
 
   const recupererCombats = async () => {
     const { data } = await axios.get(URLS.BATTLE_ALL);
     setCombats(data);
   };
 
-  useEffect( () => {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const credentials = {
+      identifiant: identifiant,
+      mdp: mdp
+    }
+
+    try {
+      await axios.post(URLS.USER_LOGIN, credentials);
+      navigate("/");
+    } catch (erreur) {
+      console.error(erreur.message);
+    }
+  }
+
+  useEffect(() => {
     recupererCombats();
   }, []);
 
   return (
     <>
       <h1>Ceci est la page d'accueil</h1>
+
+      <h2>Connexion :</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="identifiant" id="identifiant" value={identifiant} onChange={(event) => setIdentifiant(event.target.value)} />
+        <br />
+        <input type="password" name="mdp" id="mdp" value={mdp} onChange={(event) => setMdp(event.target.value)} />
+        <br />
+        <button>Se connecter</button>
+      </form>
+
+      <h2>Combats :</h2>
+
       <ul>
         {combats?.map(
           (combat, cle) => (
