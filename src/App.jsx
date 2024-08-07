b6d61717066cbc8631aefb9697143fc4270386b8
Axel <axel.ein@gmail.com>
2024-08-07 15:12:57 +0200

    [APP] implemented register route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Template from './components/templates/template.jsx';
 import Accueil from './pages/accueil.jsx';
+import Inscription from './pages/inscription.jsx';
 import Profil from './pages/profil.jsx';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Template />}>
         <Route index element={<Accueil />} />
+        <Route path="/inscription" element={<Inscription />} />
         <Route path="/profil" element={<Profil />} />
       </Route>
     </Routes>
