c1ae459dc9f6e58eff266513547f76b430b5475a
Axel <axel.ein@gmail.com>
2024-08-07 15:11:49 +0200

    [APP] implemented myAccount route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Template from './components/templates/template.jsx';
 import Accueil from './pages/accueil.jsx';
+import Profil from './pages/profil.jsx';
 
 function App() {
 
@@ -10,8 +11,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Template />}>
         <Route index element={<Accueil />} />
-        {/* <Route path="/connexion" element={<Connexion />} />
-        <Route path="/inscription" element={<Inscription />} /> */}
+        <Route path="/profil" element={<Profil />} />
       </Route>
     </Routes>
     </>
