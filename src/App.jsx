c8efdb75f212a5b44f6aa5863504b9365e4e6259
Axel <axel.ein@gmail.com>
2024-08-07 16:28:31 +0200

    [APP] renamed template route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import Template from './components/templates/Template.jsx';
 import PageAccueil from './pages/PageAccueil.jsx';
 import PageConnexion from './pages/PageConnexion.jsx';
 import PageInscription from './pages/PageInscription.jsx';
