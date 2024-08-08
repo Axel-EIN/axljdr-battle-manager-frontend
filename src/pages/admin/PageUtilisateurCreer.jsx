07deebeeae2f2317c6268763dc99521cfc3f0fcc
Axel <axel.ein@gmail.com>
2024-08-08 11:31:08 +0200

    [ADMIN CREATE USER page] implemented user form

--- src/pages/admin/PageUtilisateurCreer.jsx
+++ src/pages/admin/PageUtilisateurCreer.jsx
@@ -1,31 +1,15 @@
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { URLS } from "../../constants/urls.js";
+import FormUser from "../../components/forms/FormUser.jsx";
 
 const PageUtilisateurCreer = () => {
-  const [identifiant, setIdentifiant] = useState("");
-  const [email, setEmail] = useState("");
-  const [mdp, setMdp] = useState("");
-  const [prenom, setPrenom] = useState("");
-  const [avatar, setAvatar] = useState("");
-  const [role, setRole] = useState("user");
   const navigate = useNavigate();
 
-  const soumettreFormulaire = async (event) => {
-    event.preventDefault(); // Prévient le comportement de rechargement de page par défaut sur le boutton
-    const nouvelleUtilisateur = {
-      identifiant: identifiant,
-      email: email,
-      mdp: mdp,
-      prenom: prenom,
-      avatar: avatar,
-      role: role
-    }
-
+  const creerUtilisateurSoumis = async (utilisateurSoumis) => {
     try {
-      await axios.post( URLS.USER_CREATE, nouvelleUtilisateur, { withCredentials: true } );
-      navigate("/admin"); // Rédirection sur la page admin
+      await axios.post( URLS.USER_CREATE, utilisateurSoumis, { withCredentials: true } );
+      navigate("/admin");
     } catch (erreur) {
       console.error(erreur.message);
     }
@@ -33,34 +17,8 @@ const PageUtilisateurCreer = () => {
 
   return (
     <>
-      <h1>Page de Création d'un Utilisateur !</h1>
-
-      <form onSubmit={soumettreFormulaire}>
-
-        <label htmlFor="identifiant">Identifiant :</label>
-        <input type="text" name="identifiant" value={identifiant} onChange={(event) => setIdentifiant(event.target.value)} />
-
-        <label htmlFor="mdp">Mot de passe :</label>
-        <input type="password" name="mdp" value={mdp} onChange={(event) => setMdp(event.target.value)} />
-      
-        <label htmlFor="email">Email :</label>
-        <input type="email" name="email" value={email} onChange={(event) => setEmail(event.target.value)} />
-
-        <label htmlFor="prenom">Prénom</label>
-        <input type="text" name="prenom" value={prenom} onChange={(event) => setPrenom(event.target.value)} />
-
-        <label htmlFor="avatar">Avatar</label>
-        <input type="avatar" name="avatar" value={avatar} onChange={(event) => setAvatar(event.target.value)} />
-
-        <label htmlFor="role">Role</label>
-        <select name="role" id="role" onChange={(event) => setRole(event.target.value)} >
-          <option defaultValue value="user">Utilisateur</option>
-          <option value="mj">Maître du Jeu</option>
-          <option value="admin">Administrateur</option>
-        </select>
-
-        <button type="submit">Créer l'utilisateur</button>
-      </form>
+      <h1>CRÉATION UTILISATEUR</h1>
+      <FormUser fonctionPropsSoumissionFormulaire={creerUtilisateurSoumis} admin={true} />
     </>
   );
 };
