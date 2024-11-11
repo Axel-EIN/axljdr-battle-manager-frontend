e7af3c27a0c82825ec6d8665f42dff9397612399
Axel <axel.ein@gmail.com>
2024-11-11 02:34:50 +0100

    [HOME page] moved useEffect and included socket connection to solve an issue

--- src/pages/PageAccueil.jsx
+++ src/pages/PageAccueil.jsx
@@ -4,29 +4,25 @@ import { URLS } from '../constants/urls.js';
 import { Link } from "react-router-dom";
 import { io } from "socket.io-client"; // Importation de la lib socketIO pour le client (et non server)
 
-const socket = io("http://localhost:8080"); // Initialisation de la connexion WebSocket avec le Back-End
-
 function PageAccueil() {
   const [combats, setCombats] = useState([]);
-
-  useEffect(() => { // grâce à useEffect on monte au chargement la fonction recupererCombats
-    recupererCombats();
-
-    socket.on('newBattle', (reponseNewBattle) => { // écoute de l'évenement websocket newBattle et action si délenché
-      setCombats((prevCombats) => [...prevCombats, reponseNewBattle]);
-    });
-
-    return () => { // Nettoyage de l'écouteur d'événement newBattle lors du démontage du composant
-      socket.off('newBattle');
-    };
-  }, []);
 
   const recupererCombats = async () => {
     const { data } = await axios.get(URLS.BATTLE_ALL);
     setCombats(data);
   };
 
-  return ( // Affichage de la vue
+  useEffect(() => {
+    const socket = io("http://localhost:8080"); // Initialisation de la connexion WebSocket avec le Back-End
+    recupererCombats();
+
+    socket.on('newBattle', () => { recupererCombats(); });
+    return () => { // Nettoyage des écouteurs d'événement lors du démontage du composant
+      socket.off('newBattle');
+    };
+  }, []);
+
+  return (
     <>
       <h1>PAGE ACCUEIL</h1>
 
