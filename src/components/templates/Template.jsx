ad7db19576a11c4eba5ecdbe0cade859821d5367
Axel <axel.ein@gmail.com>
2024-11-11 04:00:48 +0100

    [TEMPLATE] fixed capitalize filename and css

--- src/components/templates/Template.jsx
+++ src/components/templates/Template.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
-import "./template.css";
+import "./Template.css";
 
 const Template = () => {
   return (
