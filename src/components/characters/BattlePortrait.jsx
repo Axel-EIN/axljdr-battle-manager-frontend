82da040de0aa61b92f017a65d144e598293bb1d9
Axel <axel.ein@gmail.com>
2024-11-12 00:13:23 +0100

    [BATTLE portrait] changed variable due to combat object structure changed in back-end

--- src/components/characters/BattlePortrait.jsx
+++ src/components/characters/BattlePortrait.jsx
@@ -1,13 +1,15 @@
-function BattlePortrait( { personnage } ) {
+function BattlePortrait( { participation } ) {
   return (
-    <div className="character" key={personnage.id}>
+    <div className="character" key={participation.Personnage.id}>
       <div className="portrait-wrapper">
-        {personnage.portrait? <img className="portrait-medium" src={`http://localhost:8080/${personnage.portrait}`}  alt={personnage.prenom} /> : <img src={'https://i.pravatar.cc/96'} alt={personnage.prenom} /> }
-        <strong>{personnage.prenom}</strong>
+        {participation.Personnage.portrait? <img className="portrait-medium" src={`http://localhost:8080/${participation.Personnage.portrait}`}  alt={participation.Personnage.prenom} /> : <img src={'https://i.pravatar.cc/96'} alt={participation.Personnage.prenom} /> }
+        <strong>{participation.Personnage.prenom}</strong>
       </div>
       <div className="stats">
-        <span>HP <strong>{personnage.HP}</strong></span>
-        <span>Initiative <strong>{personnage.Participation.initiative}</strong></span>
+        <span>HP <strong>{participation.Personnage.HP}</strong></span>
+        <span>Initiative <strong>{participation.initiative}</strong></span>
+        <span>Posture <strong>{participation.posture}</strong></span>
+        <span>A joué <strong>{participation.isPlayed? <span>Oui</span> : <span>Non</span>}</strong></span>
       </div>
     </div>
   );
