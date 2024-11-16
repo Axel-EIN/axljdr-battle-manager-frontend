b6bfab6d2484f59a06b8ef237d572b019ac00b1f
Axel <axel.ein@gmail.com>
2024-11-17 00:11:59 +0100

    [MAIN] fixed UserProvider import

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { FournisseurUtilisateur } from './contexts/contexteUtilisateur.jsx';
+import { UserProdiver } from './contexts/contexteUser.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <FournisseurUtilisateur>
+  <UserProdiver>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </FournisseurUtilisateur>,
-)
+  </UserProdiver>,
+);
