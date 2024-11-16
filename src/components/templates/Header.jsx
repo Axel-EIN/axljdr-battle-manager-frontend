9f420473610aad2af20cfe70780ac11f17ec5952
Axel <axel.ein@gmail.com>
2024-11-16 23:54:47 +0100

    [HEADER] fixed to english variables

--- src/components/templates/Header.jsx
+++ src/components/templates/Header.jsx
@@ -1,32 +1,23 @@
 import { useContext, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import { ContexteUtilisateur } from "../../contexts/contexteUtilisateur";
+import { ContexteUser } from "../../contexts/contexteUser.jsx";
 import './Header.css';
 
 function Header() {
-  // Récupération de l'utilisateur et de la fonction pour deconnecter via destructuration du contexte utilisateur
-  const { utilisateur, deconnecterUtilisateur } = useContext(ContexteUtilisateur);
+  const { user, logoutUser } = useContext(ContexteUser); // Récupération utilisateur et fonction logout via destructuration du contexte utilisateur
   const navigate = useNavigate();
 
-  useEffect(() => {}, [utilisateur]);
+  useEffect(() => {}, [user]);
 
-  const gererDeconnexion = () => {
-    deconnecterUtilisateur();
+  const handleLogout = () => {
+    logoutUser();
     navigate("/"); // Redirection à l'accueil
   };
 
   return (
     <header>
-      {utilisateur && utilisateur.role == 'admin' &&
-        (
-          <div id="adminbar"><Link to="/admin">Panneau d'Administration</Link></div>
-        )
-      }
-      {utilisateur && utilisateur.role == 'mj' &&
-        (
-          <div id="mjbar"><Link to="/mj">Panneau du Maître du Jeu</Link></div>
-        )
-      }
+      {user && user.role == 'admin' && <div id="adminbar"><Link to="/admin">Panneau d'Administration</Link></div>}
+      {user && user.role == 'gamemaster' && <div id="mjbar"><Link to="/mj">Panneau du Maître du Jeu</Link></div>}
       <div id="navbar">
         <div id="logo">
           <img src="/logo-axljdrbattle.png" alt="Logo AXL JDR Battle" />
@@ -37,11 +28,11 @@ function Header() {
           </ul>
         </nav>
         <div id="userzone">
-          {utilisateur ? (
+          {user ? (
             <div className="logged">
-              <div>Bienvenue <strong>{utilisateur.prenom}</strong> !</div>
+              <div>Bienvenue <strong>{user.firstname}</strong> !</div>
               <Link to="/mon-compte">Mon compte</Link>
-              <button onClick={gererDeconnexion}>Se deconnecter</button>
+              <button onClick={handleLogout}>Se deconnecter</button>
             </div>
           ) : (
             <div className="notlogged">
