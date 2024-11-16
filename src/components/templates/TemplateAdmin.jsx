913d60db213ed01944321528910b856e8c8431cf
Axel <axel.ein@gmail.com>
2024-11-17 00:05:08 +0100

    [TEMPLATE admin] fixed to english variables

--- src/components/templates/TemplateAdmin.jsx
+++ src/components/templates/TemplateAdmin.jsx
@@ -1,20 +1,22 @@
 import Template from "./Template.jsx";
-import { ContexteUtilisateur } from "../../contexts/contexteUtilisateur.jsx";
+import { ContexteUser } from "../../contexts/contexteUser.jsx";
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 
-const TemplateMJ = () => {
-  const { utilisateur, loading } = useContext(ContexteUtilisateur); // Récupération de l'utilsiateur connecté dans le contexte et de la variable loading
+const TemplateAdmin = () => {
+  const { user, loading } = useContext(ContexteUser); // Récupération de l'utilsiateur connecté dans le contexte et de la variable loading
 
   // Si le loading de l'utilisateur du context et terminé, alors on verifie si l'utilisateur existe.
   // Si oui on affiche le template, sinon on redirige vers la page de connexion
   return (
     <>
-      {!loading && (
-        <>{utilisateur && utilisateur.role == 'admin' ? <Template /> : <>{console.log("Désolé, vous n'êtes pas Admin, vous allez être redirigé sur la page de connexion.")}<Navigate to="/connexion" /></> }</>
-      )}
+      {!loading &&
+        <>
+          {user && user.role == 'admin' ? <Template /> : <Navigate to="/connexion" />}
+        </>
+      }
     </>
   );
 };
 
-export default TemplateMJ;
+export default TemplateAdmin;
