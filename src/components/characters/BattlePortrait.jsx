fdc3375c5a19e712269a1ae800430ba090bb9b51
Axel <axel.ein@gmail.com>
2024-11-16 23:27:33 +0100

    [BATTLE portrait] fixed with english property names

--- src/components/characters/BattlePortrait.jsx
+++ src/components/characters/BattlePortrait.jsx
@@ -2,15 +2,20 @@ import { URLS } from '../../constants/urls.js'
 
 function BattlePortrait( { participation } ) {
   return (
-    <div className="character" key={participation.Personnage.id}>
+    <div className="character" key={participation.Character.id}>
       <div className="portrait-wrapper">
-        <img className="portrait-medium" src={participation.Personnage.portrait? `${URLS.BASE_URL}/${participation.Personnage.portrait}` : 'https://i.pravatar.cc/96'}  alt={participation.Personnage.prenom} />
-        <strong>{participation.Personnage.prenom}</strong>
+        <img
+          className="portrait-medium"
+          src={participation.Character.portrait? `${URLS.BASE_URL}/${participation.Character.portrait}` : 'https://i.pravatar.cc/96'} 
+          alt={participation.Character.firstname}
+          title={participation.Character.firstname}
+        />
+        <strong>{participation.Character.firstname}</strong>
       </div>
       <div className="stats">
-        <span>HP <strong>{participation.Personnage.HP}</strong></span>
+        <span>HP <strong>{participation.Character.health}</strong></span>
         <span>Initiative <strong>{participation.initiative}</strong></span>
-        <span>Posture <strong>{participation.posture}</strong></span>
+        <span>Posture <strong>{participation.stance}</strong></span>
         <span>A joué <strong>{participation.is_played? <span>Oui</span> : <span>Non</span>}</strong></span>
       </div>
     </div>
