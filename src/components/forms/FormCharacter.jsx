1c589cce93014e3f8dfd851dcd3f147a64e2e1e6
Axel <axel.ein@gmail.com>
2024-12-02 18:55:44 +0100

    [CHARACTER form] reworked new layout

--- src/components/forms/FormCharacter.jsx
+++ src/components/forms/FormCharacter.jsx
@@ -3,63 +3,85 @@ import axios from 'axios';
 import { URLS } from '../../constants/urls.js';
 
 const FormCharacter = ({ submitPropsFunction, initialCharacter = false }) => {
-  const [lastname, setLastname] = useState(initialCharacter.lastname || '');
-  const [firstname, setFirstname] = useState(initialCharacter.firstname || '');
-  const [portrait, setPortrait] = useState(null);
-  const [illustration, setIllustration] = useState(null);
-  const [userID, setUserID] = useState(initialCharacter.user_id || '');
-  const [users, setUsers] = useState([]);
+    const [lastname, setLastname] = useState(initialCharacter.lastname || '');
+    const [firstname, setFirstname] = useState(initialCharacter.firstname || '');
+    const [portrait, setPortrait] = useState(null);
+    const [illustration, setIllustration] = useState(null);
+    const [userID, setUserID] = useState(initialCharacter.user_id || '');
+    const [users, setUsers] = useState([]);
   
-  useEffect( () => {
-    const retrieveAllUsers = async () => {
-      try {
-        const { data } = await axios.get(URLS.USER_ALL);
-        setUsers(data);
-      } catch ( { response } ) {
-        alert(response.data.error); }
-    };
-    retrieveAllUsers();
-  } , []);
+    useEffect( () => {
+        const retrieveAllUsers = async () => {
+        try {
+            const { data } = await axios.get(URLS.USER_ALL);
+            setUsers(data);
+        } catch ( { response } ) {
+            alert(response.data.error); }
+        };
+        retrieveAllUsers();
+    } , []);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const formData = new FormData(); // Création d'un objet FormData pour l'upload de fichier
-    if (lastname && lastname != '') formData.append('lastname', lastname);
-    if (firstname && firstname != '') formData.append('firstname', firstname);
-    if (portrait && portrait != '') formData.append('portrait', portrait);
-    if (illustration && illustration != '') formData.append('illustration', illustration);
-    if (userID && userID != '') formData.append('user_id', userID);
-    if ( !formData.get('lastname') || !formData.get('firstname') ) {
-      alert("Il manque des données de formulaire à remplir pour créer le personnage !");
-      return;
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const formData = new FormData(); // Création d'un objet FormData pour l'upload de fichier
+
+        if (lastname && lastname != '') formData.append('lastname', lastname);
+        if (firstname && firstname != '') formData.append('firstname', firstname);
+        if (portrait && portrait != '') formData.append('portrait', portrait);
+        if (illustration && illustration != '') formData.append('illustration', illustration);
+        if (userID && userID != '') formData.append('user_id', userID);
+        if ( !formData.get('lastname') || !formData.get('firstname') ) {
+            alert("Il manque des données de formulaire à remplir pour créer le personnage !");
+            return;
+        }
+
+        submitPropsFunction(formData);
     }
-    submitPropsFunction(formData);
-  }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="lastname">Nom :</label>
-      <input required type="text" name="lastname" value={lastname} onChange={(event) => setLastname(event.target.value)} />
+    <form className="card" onSubmit={handleSubmit}>
 
-      <label htmlFor="firstname">Prénom :</label>
-      <input required type="text" name="firstname" value={firstname} onChange={(event) => setFirstname(event.target.value)} />
+        <div className="label-input">
+            <label htmlFor="lastname">Nom :</label>
+            <input required type="text" name="lastname" value={lastname} onChange={(event) => setLastname(event.target.value)} />
+        </div>
+
+        <div className="label-input">
+            <label htmlFor="firstname">Prénom :</label>
+            <input required type="text" name="firstname" value={firstname} onChange={(event) => setFirstname(event.target.value)} />
+        </div>
     
-      {initialCharacter && initialCharacter.portrait && !portrait && <img src={`${URLS.BASE_URL}/${initialCharacter.portrait}`} />}
-      <label htmlFor="portrait">Portrait</label>
-      <input type="file" name="portrait" onChange={(event) => setPortrait(event.target.files[0])} />
+        <div className="form-row">
+            {initialCharacter && initialCharacter.portrait && !portrait &&
+                <img className="portrait" src={`${URLS.BASE_URL}/${initialCharacter.portrait}`} />}
+            <div className="label-input">
+                <label htmlFor="portrait">Portrait</label>
+                <input type="file" name="portrait" onChange={(event) => setPortrait(event.target.files[0])} />
+            </div>
+        </div>
 
-      {initialCharacter && initialCharacter.illustration && !illustration && <img src={`${URLS.BASE_URL}/${initialCharacter.illustration}`} />}
-      <label htmlFor="illustration">Illustration :</label>
-      <input type="file" name="illustration" onChange={(event) => setIllustration(event.target.files[0])} />
+        <div className="form-row">
+            {initialCharacter && initialCharacter.illustration && !illustration &&
+                <img className="illustration" src={`${URLS.BASE_URL}/${initialCharacter.illustration}`} />}
+            <div className="label-input">
+                <label htmlFor="illustration">Illustration :</label>
+                <input type="file" name="illustration" onChange={(event) => setIllustration(event.target.files[0])} />
+            </div>
+        </div>
 
-      <label htmlFor="user_id">Joueur :</label>
-      <select name="user_id" id="user_id" value={userID} onChange={(event) => setUserID(event.target.value)} >
-        <option>Aucun / PNJ (Maître du Jeu)</option>
-        {users.map( (user) =>
-          <option key={user.id} value={user.id}>{user.firstname}</option> )}
-      </select>
+        <div className="label-input">
+            <label htmlFor="user_id">Joueur :</label>
+            <select name="user_id" id="user_id" value={userID} onChange={(event) => setUserID(event.target.value)} >
+                <option>Aucun / PNJ (Maître du Jeu)</option>
+                {users.map( (user) =>
+                <option key={user.id} value={user.id}>{user.firstname}</option> )}
+            </select>
+        </div>
 
-      <button type="submit">{!initialCharacter ? <span>Créer le personnage</span> : <span>Modifier le personnage</span>}</button>
+        <button className="btn-primary btn-large" type="submit">
+            {!initialCharacter ? <span>Créer le personnage</span> : <span>Modifier le personnage</span>}
+        </button>
+
     </form>
   )
 }
