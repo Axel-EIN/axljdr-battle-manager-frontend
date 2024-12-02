f66d450ac2487d347e8a6ae79184954ec58f269c
Axel <axel.ein@gmail.com>
2024-12-02 11:47:30 +0100

    [GAMEMASTER page] reworked new layout

--- src/pages/gamemaster/GameMasterPage.jsx
+++ src/pages/gamemaster/GameMasterPage.jsx
@@ -3,8 +3,8 @@ import { URLS } from '../../constants/urls.js';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
+import { GrEdit } from "react-icons/gr";
+import { FaRegTrashCan } from "react-icons/fa6";
 
 const GameMasterPage = () => {
     const [characters, setCharacters] = useState([]);
@@ -13,16 +13,19 @@ const GameMasterPage = () => {
     const navigate = useNavigate();
 
     useEffect(() => {
+
         const retrieveAllCharacters = async () => {
             // Try Catch ?
             const { data } = await axios.get(URLS.CHAR_ALL);
             setCharacters(data);
         }
+
         const retrieveAllBattles = async () => {
             // Try Catch ?
             const { data } = await axios.get(URLS.BATTLE_ALL);
             setBattles(data);
         }
+        
         retrieveAllCharacters();
         retrieveAllBattles();
         setReload(false);
@@ -50,67 +53,72 @@ const GameMasterPage = () => {
 
     return (
         <>
-            <h1>Le Paneau du Maître du Jeu</h1>
+            <h1>Panneau du Maître du Jeu</h1>
 
-            <h2>characters :</h2>
-            <button onClick={() => navigate("/mj/personnage/creer")}>Créer un Personnage</button>
+            <div className="layout-flex-row">
+                <div className="list-characters">
 
-            <table>
-                <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nom</th>
-                    <th>Prenom</th>
-                    <th>Portrait</th>
-                    <th>Illustration</th>
-                    <th>Joueur</th>
-                    <th>Modifier</th>
-                    <th>Supprimer</th>
-                </tr>
-                </thead>
-                <tbody>
-                {characters.map(character =>
-                    <tr key={character.id}>
-                        <td>{character.id}</td>
-                        <td>{character.name}</td>
-                        <td>{character.firstname}</td>
-                        <td>{character.portrait? <img src={`${URLS.BASE_URL}/${character.portrait}`} /> : <img src={'https://i.pravatar.cc/96'} /> }</td>
-                        <td>{character.illustration? <img src={`${URLS.BASE_URL}/${character.illustration}`} /> : <img src={'https://i.pravatar.cc/96'} /> }</td>
-                        <td>{character.User?.firstname}</td>
-                        <td><Link to={'/mj/personnage/modifier' + '/' + character.id}><EditIcon /></Link></td>
-                        <td><Link><DeleteIcon onClick={() => deleteCharacter(character.id)} /></Link></td>
-                    </tr>
-                )}
-                </tbody>
-                <tfoot></tfoot>
-            </table>
+                    <h2>Personnages</h2>
+                    <button className="btn-primary btn-medium" onClick={() => navigate("/mj/personnage/creer")}>Ajouter un Personnage</button>
 
-            <h2>Combats :</h2>
-            <button onClick={() => navigate("/mj/combat/creer")}>Créer un Combat</button>
+                    <div className="flex-table">
+                        <div className="grid-row fr15 head">
+                            <div className="span2">Portrait</div>
+                            <div className="span2">Illustration</div>
+                            <div className="span3">Nom</div>
+                            <div className="span3">Prénom</div>
+                            <div className="span3">Joueur</div>
+                            <div></div>
+                            <div></div>
+                        </div>
 
-            <table>
-                <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Titre</th>
-                    <th>Statut</th>
-                    <th>Modifier</th>
-                    <th>Supprimer</th>
-                </tr>
-                </thead>
-                <tbody>
-                {battles.map(battle =>
-                    <tr key={battle.id}>
-                        <td>{battle.id}</td>
-                        <td>{battle.title}</td>
-                        <td>{battle.status}</td>
-                        <td><Link to={'/mj/combat/modifier' + '/' + battle.id}><EditIcon /></Link></td>
-                        <td><Link><DeleteIcon onClick={() => deleteBattle(battle.id)} /></Link></td>
-                    </tr>
-                )}
-                </tbody>
-                <tfoot></tfoot>
-            </table>
+                        {characters.map(character =>
+                            <div className="grid-row fr15 card row" key={character.id}>
+                                <div className="span2">
+                                    {character.portrait?
+                                        <img className="portrait small" src={`${URLS.BASE_URL}/${character.portrait}`} />
+                                        : <img className="portrait small" src={'https://i.pravatar.cc/96'} /> }
+                                </div>
+                                <div className="span2">{character.illustration? <img className="illustration small" src={`${URLS.BASE_URL}/${character.illustration}`} /> : <img className="illustration small" src={'https://i.pravatar.cc/96'} /> }</div>
+                                <div className="span3">{character.lastname}</div>
+                                <div className="span3">{character.firstname}</div>
+                                <div className="span3">{character.User?.firstname}</div>
+                                <div><Link to={'/mj/personnage/modifier' + '/' + character.id}><GrEdit /></Link></div>
+                                <div><Link><FaRegTrashCan onClick={() => window.confirm("Êtes-vous sûr de vouloir supprimer ce personnage ?") && deleteCharacter(character.id)} /></Link></div>
+                            </div>
+                        )}
+                    </div>
+
+                </div>
+
+                <div className="list-battles">
+
+                    <h2>Combats</h2>
+                    <button className="btn-primary btn-medium" onClick={() => navigate("/mj/combat/creer")}>Créer un Combat</button>
+
+                    <div className="flex-table">
+
+                        <div className="grid-row fr7 head">
+                            <div className="span3">Titre</div>
+                            <div className="span2">Statut</div>
+                            <div></div>
+                            <div></div>
+                        </div>
+
+                        {battles.map(battle =>
+                            <div className="grid-row fr8 card row" key={battle.id}>
+                                <div className="span4">{battle.title}</div>
+                                <div className="span2">{battle.status}</div>
+                                <div><Link to={'/mj/combat/modifier' + '/' + battle.id}><GrEdit /></Link></div>
+                                <div><button className="btn-icon" onClick={() => window.confirm("Êtes-vous sûr de vouloir supprimer ce combat ?") && deleteBattle(battle.id)}><FaRegTrashCan /></button></div>
+                            </div>
+                        )}
+
+                    </div>
+
+                </div>
+
+            </div>
         </>
     );
 };
