2ba690cfa90a6be872e747a063adf83306a5a0cd
Axel <axel.ein@gmail.com>
2024-12-04 01:37:36 +0100

    [BATTLE portrait] reworked new layout

--- src/components/characters/BattlePortrait.jsx
+++ src/components/characters/BattlePortrait.jsx
@@ -1,26 +1,44 @@
-import { URLS } from '../../constants/urls.js'
+import { URLS } from '../../constants/urls.js';
+import { FaHeartPulse } from "react-icons/fa6";
+import { FaRunning } from "react-icons/fa";
+import { IoSpeedometerSharp } from "react-icons/io5";
+import classNames from 'classnames';
 
-function BattlePortrait( { participation } ) {
-  return (
-    <div className="character" key={participation.Character.id}>
-      <div className="portrait-wrapper">
-        <img
-          className="portrait-medium"
-          src={participation.Character.portrait? `${URLS.BASE_URL}/${participation.Character.portrait}` : 'https://i.pravatar.cc/96'} 
-          alt={participation.Character.firstname}
-          title={participation.Character.firstname}
-        />
-        <strong>{participation.Character.firstname}</strong>
-      </div>
-      <div className="stats">
-        <span>HP <strong>{participation.Character.health}</strong></span>
-        <span>Initiative <strong>{participation.initiative}</strong></span>
-        <span>Posture <strong>{participation.stance}</strong></span>
-        <span>TN <strong>{participation.current_tn}</strong></span>
-        <span>A joué <strong>{participation.is_played? <span>Oui</span> : <span>Non</span>}</strong></span>
-      </div>
-    </div>
-  );
+function BattlePortrait( { participation, key, isActive = false, isPlayed = false, isImpacted1 = false, isImpacted2 = false } ) {
+    return (
+        <div key={key} className={classNames('character-wrapper', {'impact1' : isImpacted1, 'impact2' : isImpacted2,})} >
+            <div className={participation.is_out ? 'slash visible' : 'slash hidden'}><img src="/ui/out.png" /></div>
+            <div className={classNames('character card', {
+                'active': isActive,
+                'played': isPlayed,
+                'teamA' : participation.team === 1,
+                'teamB' : participation.team === 2,
+                'out' : participation.is_out,
+                'impact1' : isImpacted1,
+                'impact2' : isImpacted2,
+            })} key={participation.Character.id}>
+                <div className={classNames('firstname', {
+                    'active': isActive,
+                    'teamA' : participation.team === 1,
+                    'teamB' : participation.team === 2,
+                })} >{participation.Character.firstname}</div>
+                <div className="portrait-wrapper">
+                    <img
+                        className="portrait medium"
+                        src={participation.Character.portrait? `${URLS.BASE_URL}/${participation.Character.portrait}` : 'https://i.pravatar.cc/96'} 
+                        alt={participation.Character.firstname}
+                        title={participation.Character.firstname}
+                    />
+                </div>
+                <div  className={classNames('stats', {'hidden' : participation.is_out})}>
+                    <div className="stat-row"><FaHeartPulse className='heart' /> HP <strong className="value">{participation.Character.health}</strong></div>
+                    <div className="stat-row"><FaRunning /> Evasion <strong className="value">{participation.current_tn}</strong></div>
+                    <div className="stat-row"><IoSpeedometerSharp /> Initiative <strong className="value">{participation.initiative}</strong></div>
+                    <div className="stat-row">Posture <strong className="value">{participation.stance}</strong></div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default BattlePortrait;
