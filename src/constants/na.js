b9c1b319e865e4e4ead7020542c3e78edeb6887f
Axel <axel.ein@gmail.com>
2024-12-06 15:21:31 +0100

    [CONSTANTS na] added NA constants for non available avatar, portrait and illustration

---
+++ src/constants/na.js
@@ -0,0 +1,10 @@
+const NA_FOLDER = '/na';
+
+export const NA = {
+    // USER
+    AVATAR: `${NA_FOLDER}/na_avatar.png`,
+
+    // CHARACTER
+    PORTRAIT: `${NA_FOLDER}/na_portrait.jpg`,
+    ILLUSTRATION: `${NA_FOLDER}/na_illustration.jpg`,
+}
