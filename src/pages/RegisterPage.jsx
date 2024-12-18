9ba0634414406be832dac7afff983a2ae41fc05a
Axel <axel.ein@gmail.com>
2024-12-18 02:21:35 +0100

    [REGISTER page] edited the alert message

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -4,29 +4,29 @@ import { URLS } from "../constants/urls.js";
 import FormUser from "../components/forms/FormUser.jsx";
 
 const RegisterPage = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const submitPropsRegisterUser = async (formData) => {
-    try {
-      const newUser = {
-        login: formData.get('login'),
-        password: formData.get('password'),
-        email: formData.get('email'),
-        firstname: formData.get('firstname')
-      }
-      await axios.post(URLS.USER_REGISTER, newUser);
-      alert("Inscription réussie ! Vous pouvez vous connecter !");
-      navigate("/");
-    } catch ({response}) {
-      alert(response.data.error); }
-  }
+    const submitPropsRegisterUser = async (formData) => {
+        try {
+            const newUser = {
+                login: formData.get('login'),
+                password: formData.get('password'),
+                email: formData.get('email'),
+                firstname: formData.get('firstname')
+            }
+            await axios.post(URLS.USER_REGISTER, newUser);
+            alert("Inscription réussie ! Merci de confirmer votre email en allons dans votre boîte mail !");
+            navigate("/");
+        } catch ({response}) {
+            alert(response.data.error); }
+    }
 
-  return (
-    <>
-      <h1>INSCRIPTION</h1>
-      <FormUser submitPropsFunction={submitPropsRegisterUser} register={true} />
-    </>
-  );
+    return (
+        <>
+            <h1>INSCRIPTION</h1>
+            <FormUser submitPropsFunction={submitPropsRegisterUser} register={true} />
+        </>
+    );
 };
 
 export default RegisterPage;
