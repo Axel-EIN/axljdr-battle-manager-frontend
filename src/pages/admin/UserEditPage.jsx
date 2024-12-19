931e1198f4c056da9be36f7f62eddcaa10565097
Axel <axel.ein@gmail.com>
2024-12-19 22:03:20 +0100

    [ADMINUSEREDIT page] fixed formatting

--- src/pages/admin/UserEditPage.jsx
+++ src/pages/admin/UserEditPage.jsx
@@ -5,36 +5,45 @@ import { URLS } from '../../constants/urls.js';
 import FormUser from "../../components/forms/FormUser.jsx";
 
 const UserEditPage = () => {
-  const { utilisateurID } = useParams();
-  const navigate = useNavigate();
-  const [ userToEdit, setUserToEdit ] = useState(null);
+    const { utilisateurID } = useParams();
+    const navigate = useNavigate();
+    const [ userToEdit, setUserToEdit ] = useState(null);
 
-  useEffect( () => {
-    const retrieveOneUser = async (ID) => {
-      const { data } = await axios.get(URLS.USER_ONE + '/' + ID);
-      setUserToEdit(data);
+    const getUser = async (ID) => {
+        try {
+            const { data } = await axios.get(URLS.USER_ONE + '/' + ID);
+            setUserToEdit(data);
+        } catch(error) {
+            console.log(error);
+            alert(error);
+        }
     }
-    retrieveOneUser(utilisateurID);
-  }, []);
 
-  const submitPropsEditUser = async (formData) => {
-    try {
-      await axios.put( URLS.USER_EDIT + '/' + utilisateurID, formData, {
-        headers: { 'Content-Type': 'multipart/form-data' },
-        withCredentials: true
-      });
-      navigate("/admin"); // Rédirection sur la page admin
-      alert("L'Utilisateur a bien été modifié !");
-    } catch ({response}) {
-      alert(response.data.error); }
-  }
+    useEffect(() => {
+        getUser(utilisateurID);
+    }, []);
 
-  return (
-    <>
-      <h1>Modifier l'utilisateur</h1>
-      {userToEdit ? <FormUser submitPropsFunction={submitPropsEditUser} initialUser={userToEdit} /> : <p>Chargement des données de l'utilisateur...</p>}
-    </>
-  );
+    const submitPropsEditUser = async (formData) => {
+        try {
+            await axios.put( URLS.USER_EDIT + '/' + utilisateurID, formData,
+                {
+                    headers: { 'Content-Type': 'multipart/form-data' },
+                    withCredentials: true,
+                }
+            );
+            navigate("/admin");
+            alert("L'Utilisateur a bien été modifié !");
+        } catch ({response}) {
+            alert(response.data.error);
+        }
+    }
+
+    return (
+        <>
+            <h1>Modifier l'utilisateur</h1>
+            {userToEdit ? <FormUser submitPropsFunction={submitPropsEditUser} initialUser={userToEdit} /> : <p>Chargement des données de l'utilisateur...</p>}
+        </>
+    );
 };
 
 export default UserEditPage;
